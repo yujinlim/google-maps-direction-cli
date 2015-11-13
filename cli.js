ecdb59d4560d7b24b7ac1d36e8456f1d2045b8b2
Yu-Jin <adamlyj89@gmail.com>
2015-11-13 13:52:34 +0800

    update cli with api key for better traffic information

--- cli.js
+++ cli.js
@@ -4,9 +4,15 @@
 var meow = require('meow');
 var chalk = require('chalk');
 var direction = require('./');
-var options = {};
+var options = {
+  key: process.env.DIRECTION_API_KEY
+};
 
-var cli = meow('Usage \n $ direction <origin> <destination> \n\nExamples \n $ direction "bukit damansara" "klcc" \n');
+var cli = meow('Usage \n $ direction <origin> <destination> \n\nOptions \n $ -k, --key Include api key for better traffic forecast (or DIRECTION_API_KEY environment variable) \n\nExamples \n $ direction "bukit damansara" "klcc" \n $ DIRECTION_API_KEY=API_KEY direction "bukit damansara" "klcc" \n $ direction "bukit damansara" "klcc" -k API_KEY', {
+  alias: {
+    k: 'key'
+  }
+});
 
 if (cli.input.length === 0 || cli.input.length < 2) {
   console.log(cli.help);
@@ -19,6 +25,18 @@ if (cli.input[0]) {
 
 if (cli.input[0]) {
   options.destination = cli.input.shift();
+}
+
+if (cli.flags.key) {
+  var key = cli.flags.key
+  if (typeof(key) !== 'string') {
+    console.error(chalk.red.bold('api key is wrong type'));
+  }
+  options.key = key;
+}
+
+if (options.key) {
+  options.departure_time = 'now';
 }
 
 direction(options)
